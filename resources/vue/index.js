00d8f49d2ac7d191cb990fcc29617164270105e0
Craig Riley <c.riley@evomark.co.uk>
2024-10-05 18:19:21 +0100

    **BugFix**: Not loading fallback messages

--- resources/vue/index.js
+++ resources/vue/index.js
@@ -4,57 +4,91 @@ import { nextTick } from "vue";
 
 let i18n;
 const lang = import.meta.glob("inertia-i18n-files/**/*.json", {
-    eager: false,
+	eager: false,
 });
 
 const resolveLang = (locale) => {
-    const key = Object.keys(lang).find((file) => {
-        const filename = file.replace(/^.*[\\/]/, "");
-        return filename === `php_${locale}.json`;
-    });
-    return lang[key];
+	const key = Object.keys(lang).find((file) => {
+		const filename = file.replace(/^.*[\\/]/, "");
+		return filename === `php_${locale}.json`;
+	});
+	return lang[key];
 };
 
 export default (props) => {
-    let currentLocale = props.initialPage.props.i18n.current;
+	let preload = null,
+		preloadFallback = null;
+	let currentLocale = props.initialPage.props.i18n.current;
+	const fallbackLocale = props.initialPage.props.i18n.default;
 
-    return {
-        install: (app, config = {}) => {
-            const isLegacy = config.legacy === true;
+	return {
+		load: async () => {
+			const promises = [resolveLang(currentLocale)()];
+			if (currentLocale !== fallbackLocale) {
+				promises.push(resolveLang(fallbackLocale)());
+			}
+			return Promise.all(promises).then(([current, fallback]) => {
+				if (current) preload = current.default;
+				if (fallback) preloadFallback = fallback.default;
+				return true;
+			});
+		},
+		install: (app, config = {}) => {
+			const isLegacy = config.legacy === true;
 
-            i18n = createI18n({
-                legacy: isLegacy,
-                locale: currentLocale,
-                fallbackLocale: props.initialPage.props.i18n.default,
-            });
-            loadLocaleMessages(currentLocale);
-            app.use(i18n);
+			i18n = createI18n({
+				legacy: isLegacy,
+				locale: currentLocale,
+				fallbackLocale,
+			});
 
-            router.on("navigate", async (ev) => {
-                const newLocale = ev.detail.page.props.i18n.current;
-                if (newLocale !== currentLocale) {
-                    if (!i18n.global.availableLocales.includes(newLocale)) {
-                        await loadLocaleMessages(newLocale);
-                    }
-                    setLanguage(newLocale);
-                    currentLocale = newLocale;
-                }
-            });
-        },
-    };
+			if (preload) {
+				i18n.global.setLocaleMessage(currentLocale, preload);
+			} else {
+				loadLocaleMessages(currentLocale);
+			}
+
+			if (currentLocale !== fallbackLocale) {
+				if (preloadFallback) {
+					i18n.global.setLocaleMessage(fallbackLocale, preloadFallback);
+				} else {
+					loadLocaleMessages(fallbackLocale);
+				}
+			}
+			app.use(i18n);
+
+			router.on("navigate", async (ev) => {
+				const newLocale = ev.detail.page.props.i18n.current;
+				if (newLocale !== currentLocale) {
+					if (!i18n.global.availableLocales.includes(newLocale)) {
+						await loadLocaleMessages(newLocale);
+					}
+					setLanguage(newLocale);
+					currentLocale = newLocale;
+				}
+			});
+		},
+	};
 };
 
+/**
+ * @param { string } newLocale The locale to switch to
+ */
 function setLanguage(newLocale) {
-    if (i18n.mode === "legacy") {
-        i18n.global.locale = newLocale;
-    } else {
-        i18n.global.locale.value = newLocale;
-    }
+	if (i18n.mode === "legacy") {
+		i18n.global.locale = newLocale;
+	} else {
+		i18n.global.locale.value = newLocale;
+	}
 }
 
+/**
+ * @param { string } locale The locale to load
+ * @returns { Promise } The next application tick
+ */
 async function loadLocaleMessages(locale) {
-    const messages = await resolveLang(locale)();
-    i18n.global.setLocaleMessage(locale, messages.default);
+	const messages = await resolveLang(locale)();
+	i18n.global.setLocaleMessage(locale, messages.default);
 
-    return nextTick();
+	return nextTick();
 }
